757544285e6ce0b6b4cb4d47d9f762318f1674f7
mpapa05 <kisgibbon@gmail.com>
2024-06-26 11:37:14 +0200

    create project skeleton

---
+++ openlayers-demo-angular/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  title = 'openlayers-demo-angular';
+
+  map: Map | undefined;
+
+  ngOnInit(): void {
+    this.map = new Map({
+      view: new View({
+        center: [0, 0],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'ol-map'
+    });
+  }
+}
